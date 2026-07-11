bae8b38307b0ab8e53f6c1b7fe2acea84b876159
Priyanka <priyankaraghochaten@gmail.com>
2026-07-11 23:25:01 +0530

    function

---
+++ function.js
@@ -0,0 +1,153 @@
+
+// function declaration
+function fun(){
+    console.log("hello world");
+}
+  fun()
+
+
+// functoin expression
+let hel= function(){
+    console.log("heyyyyy")
+}
+hel()
+
+// fat arrow function
+let fan = () => {
+    console.log("hmmmm")
+}
+fan()
+
+function dance(v1){
+    console.log(`${v1} is dancing`);
+}
+dance("donkey");
+dance("monkey");
+
+// default parameter
+function add(v1=0,v2=0){
+    console.log(v1 + v2);
+}
+add(1,2);
+
+// jab argument kai saare value ho to humein utna hi para 
+// meter lana padta hai isse bachna ke liya hu rest ka use
+//  karte hai ... agar ... function ke parameter space mein
+//  lage to wo rest operator hai and agar wo arrays and objects
+//  mein lade to wo spread operator hai
+function abcd(...val ){
+    console.log(val)
+}
+abcd(1,2,3,4,5,6,7,8,9);
+
+// return and early return
+    function abcd(v){
+        return 12 +v;
+    }
+    let val = abcd(23);
+    console.log(val);
+
+// first class function --> function ko value ki tarah treat kar sakte ho
+function abce(value){
+value();
+}
+abce(function(){
+    console.log("hey");
+})
+
+// high order function   aise fun jo ki return kare ak function ya fir 
+// accept kara ak function apna ak parameter me
+function asdf(){
+    return function(){
+        console.log("hello");
+    }
+}
+asdf()();
+
+// pure and impure functions  
+
+//  jo bhar ki value ko na badle vo pure function
+let a =20;
+function first(){ 
+    console.log("heyhey")
+}
+first()
+// jo bhar ki value ko badal de vo impure function
+function hui(){
+    a++;
+    console.log(a)
+}
+hui()
+
+
+// closures  ek function jo return kare ek or function 
+// aur return hone waala function hamesha  use karega
+//  parent func ka koi variable
+function second(){
+    let a =12;
+    return function(){
+        console.log(a);
+    }
+}
+second();
+
+
+// lexical scoping
+function third(){
+    let a =12;
+    function fourth(){
+        let b =23;
+        function fifth(){
+            let c =34;
+        }
+    }
+}
+
+
+// IIFE(immediately invoked function expression)
+(function(){
+console.log("asdf")
+})();
+
+
+// hoisting differences between declaration and expresssion
+
+// function declaration me hosting work karti hai
+sixth()
+
+function sixth(){
+    console.log("sixth");
+}
+
+// not in finction expression -> error ayega 
+// seventh();
+
+
+// let seventh = function(){
+//     console.log("seventh")
+// }
+
+
+// Q1. what is the difference between function 
+// declaration and expression in terms of hosting
+// 113-128 tak 
+
+
+// Q2. convert a function into a arrow function
+function multiply(a,b){
+    return a * b;
+}
+
+let multiply1 =(a,b) => {
+    return a * b;
+};
+
+// identify parameter and argument
+function welcome(name){
+    console.log(name);
+}
+welcome("priyanka")
+
+
+
+
--- loop.js
+++ loop.js
@@ -56,19 +56,21 @@ for (let i=1 ; i<=10 ; i++){
     console.log(`5 * ${i} = ${5*i }` );  
 }
 
+// sun of first 100 num
 let sum = 0;
 for(let i=1 ; i<=100 ;i++){
 sum = sum + i ;
 }
 console.log(sum); 
 
-
+// num divisible by 3
 for(let i = 1; i<=50 ; i++){
     if(i%3===0){
         console.log(i);
     }
 }
 
+// print number is odd or event from 1 to 100
 let val = prompt("give me a number");
 for (let i=1 ;i<=val ; i++){
     if(i%2 ===0 ){
@@ -79,9 +81,61 @@ console.log(`${i} is odd`);
     
 }
 
-
+// numbers divisible by 3 and 5
 for(let i=1; i<=100;i++){
     if( i%3 ===0 && i%5 ===0){
         console.log(i);
     }
 }
+
+// write a loop from 1 to 100 that :
+//    . print each number 
+//    . stop completely when it finds the first number divisible by 7
+
+// yaha par 7 se phale tak ka no print honge
+for(let i=1 ; i<=100 ; i++){
+    if(i%7 ===0){
+        break;
+    }
+    console.log(i);
+}
+
+// 7 bi print hoga
+for(let i=1 ; i<=100 ; i++){
+    console.log(i);
+    if(i%7 ===0){
+        break;
+    }
+    
+}
+
+// write a loop from 1 to 20 that :
+//     . skips number divisible by 3 
+//     . print all others 
+
+//     use cindition 
+for(let i=1; i<=20 ;i++){
+    if(i%3 ===0) continue;
+        
+        console.log(i)
+    
+}
+
+
+// print first 5 odd numbers only 
+// write a llop frm 1 to 100 that 
+//     . print only 5 odd numbers 
+//     . then stops the loop 
+//     use both if continue and a counter + break 
+let count = 0;
+for(let i = 1; i<=100; i++){
+    if(i%2 ===1 ){
+count++;
+console.log(i);
+    }
+    if(count===5) break;
+}
+
+
+
+
