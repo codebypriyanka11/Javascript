9a467910552d20f78e3ddfe25da2f23fa738ee44
Priyanka <priyankaraghochaten@gmail.com>
2026-07-16 16:25:01 +0530

    obj question

--- objrct.js
+++ objrct.js
@@ -81,34 +81,66 @@ let information = {
     [role]: "priyanka",
 }
 
+// QUESTION AND ANSWER
+// Q. CREATE AN OBJ FOR A STUDENT WITH NAME AGE AND ISENROLLED
+let student ={
+    name : "priyanka",
+    age : 21,
+    isenrolled : "yes",
+}
+// can an obj key be a number or boolean ? try this
+const condition ={
+    true : "yes",
+    42 : "answer",
+}
+console.log(condition[42]);
 
+// Access the value of "first-name from this object"
+const fime ={
+    "first-name":"priyanka"
+}
+console.log(fime["first-name"]);
 
+// given a dynamic key let key ="age" , how will you access user[key]?
+let key ="age";
+let dynamic ={
+    name : "priyanka",
+    age : 21,
+}
+console.log(dynamic[key]);
 
+// destructure the city and lat from the location object
+  const locations = {
+        city : "sonipat",
+        pin : 131001,
+        coordinate :{
+            lat1:23.2,
+            lng1:77.4,
+        },
+    }
 
+let { city } = locations; 
+let {lng1} = locations.coordinate;
+console.log(city);
+console.log(lng1);
 
+// destruction the key "first-name"as a variable called firstname
+const intro = {
+    "first-name" : "priyanka",
+};
+let {"first-name" : firstName} = intro;
+console.log(firstName);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// user Object.entries()to print all key-value pairs as:
+// title:javascript
+// duration:4 weeks
+const course ={
+    title : "Javascript",
+    duration : "4 weeks",
+};
+Object.entries(course).forEach(function(val){
+    console.log(val[0]+":"+val[1]);
+});
 
 
 
